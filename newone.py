ea4f35cc20e39482d82593b38b0a0c6cb69dc2ec
eladtal <taliaz122@gmail.com>
2023-02-17 18:11:03 +0200

    "brisque"

--- newone.py
+++ newone.py
@@ -2,9 +2,9 @@ from brisque import BRISQUE
 
 def print_hi():
     brisq = BRISQUE()
-    print('yaelbit_before image: %s' % brisq.get_score("yaelbit_before.jpg"))
-    print('yaelbit_afterblurr image: %s' % brisq.get_score("yaelbit_afterblurr.png"))
-    print('yaelbit image: %s' % brisq.get_score("yaelbit.png"))
+    print('yaelbit_before image: %s' % brisq.score("yaelbit_before.jpg"))
+    #print('yaelbit_afterblurr image: %s' % brisq.get_score("yaelbit_afterblurr.png"))
+    #print('yaelbit image: %s' % brisq.get_score("yaelbit.png"))
 
 
 if __name__ == '__main__':
